314ca8e43c395edef1452d8bffc43ccd1400b29d
Leizhi <leizhifesker@gmail.com>
2014-01-02 23:41:29 +0800

    fixed 2.44

--- src/com/mooo/mycoz/common/StringUtils.java
+++ src/com/mooo/mycoz/common/StringUtils.java
@@ -990,4 +990,22 @@ public class StringUtils {
 		
 		return buffer.getBytes().length;
 	}
+	
+	public static boolean isDouble(String str){
+		try {
+			Double.parseDouble(str);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+	
+	public static boolean isInteger(String str){
+		try {
+			Integer.parseInt(str);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
 }
